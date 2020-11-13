dfd18358acbeae5949b3c374f8c169fcdbe16445
Umar Hansa <umar.hansa@gmail.com>
2020-11-13 21:57:46 +0000

    port

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import config from 'config';
 
 import appConfig from './config/main-config.js';
 import routeConfig from './config/route-config.js';
 import errorConfig from './config/error-config.js';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || config.get('port');
 
 appConfig(app, express);
 routeConfig(app);
