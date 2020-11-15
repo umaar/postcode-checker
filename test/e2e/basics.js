9202020ed7f9928143412545b46af16be15b16cc
Umar Hansa <umar.hansa@gmail.com>
2020-11-15 23:17:04 +0000

    e2e tests

--- test/e2e/basics.js
+++ test/e2e/basics.js
@@ -1,14 +1,11 @@
 import test from 'ava';
-import config from 'config';
 import withPage from './_with-page.js';
-
-const port = config.get('port');
-const baseLocalURL = `http://localhost:${port}`;
 
 test.before(async t => {
 	// eslint-disable-next-line node/no-unsupported-features/es-syntax
-	const {default: serverPromise} = await import('../../src/server/index.js');
-	const server = await serverPromise;
+	const {default: startServer} = await import('../../src/server/index.js');
+	const server = await startServer;
+	t.context.baseURL = `http://localhost:${server.address().port}`;
 	t.context.server = server;
 });
 
@@ -25,12 +22,12 @@ async function submitPostcode({postcode, page}) {
 }
 
 test('page title should be correct', withPage, async (t, page) => {
-	await page.goto(baseLocalURL);
+	await page.goto(t.context.baseURL);
 	t.is(await page.title(), 'Postcode Checker', 'Page title is correct');
 });
 
 test('Searching for a postcode within the service area', withPage, async (t, page) => {
-	await page.goto(baseLocalURL);
+	await page.goto(t.context.baseURL);
 	await submitPostcode({
 		postcode: 'SE1 7QD',
 		page
@@ -40,11 +37,35 @@ test('Searching for a postcode within the service area', withPage, async (t, pag
 });
 
 test('Searching for an invalid postcode', withPage, async (t, page) => {
-	await page.goto(baseLocalURL);
+	await page.goto(t.context.baseURL);
 	await submitPostcode({
 		postcode: 'aaa',
 		page
 	});
 	const message = await page.$eval('.message', element => element.textContent);
 	t.is(message, 'The postcode "aaa" is not in the service area');
+});
+
+test('Searching for multiple postcodes', withPage, async (t, page) => {
+	await page.goto(t.context.baseURL);
+	await submitPostcode({
+		postcode: 'SH24 1AB',
+		page
+	});
+	const message1 = await page.$eval('.message', element => element.textContent);
+	t.is(message1, 'The postcode "SH24 1AB" is in the service area', 'Message confirms postcode is in the service area');
+
+	await submitPostcode({
+		postcode: 'N2 00 AG',
+		page
+	});
+	const message2 = await page.$eval('.message', element => element.textContent);
+	t.is(message2, 'The postcode "N2 00 AG" is not in the service area', 'Message confirms postcode is not in the service area');
+
+	await submitPostcode({
+		postcode: ' SE1 7QA ',
+		page
+	});
+	const message3 = await page.$eval('.message', element => element.textContent);
+	t.is(message3, 'The postcode " SE1 7QA " is in the service area', 'Message confirms postcode is in the service area');
 });
